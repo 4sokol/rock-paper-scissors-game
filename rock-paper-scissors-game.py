40655f36450d91f4ca8a7653d3818f5a72cc800f
4sokol <sokolenko88@gmail.com>
2023-12-17 15:25:32 +0100

    code finalize

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,12 +1,33 @@
-# import Libraries, Lists or Methods, usually import happens in the beginning of the file
 import random
-
-# variables & functions - is the peace of code which runs only if it was called, we define the function with 'def'
-# 'inputs' help you to interract with the user
 
 def get_choices():
     player_choice = input('Enter your choice (rock, paper or scissors): ')
-    computer_choice = 'paper'
+    options = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(options)
     choices = {'player': player_choice, 'computer': computer_choice}
-
     return choices
+
+def check_win(player, computer):
+    print('You chose '+ player + ', Computer chose ' + computer)
+#   print(f'You chose {player} , Computer chose {computer}')    the same strang as above, with 'f' string
+    if player == computer:
+        return 'It is a Tie!'
+    elif player == 'rock': 
+        if computer == 'scissors':
+            return 'You win! Rock smashes Scissors!'
+        else:
+            return 'You lose! Paper covers Rock!'
+    elif player == 'paper': 
+        if computer == 'rock':
+            return 'You win! Paper covers Rock!'
+        else:
+            return 'You lose! Scissors cut Paper!'
+    elif player == 'scissors': 
+        if computer == 'paper':
+            return 'You win! Scissors cut Paper!!'
+        else:
+            return 'You lose! Rock smashes Scissors!'
+
+choices = get_choices()
+result = check_win(choices['player'], choices['computer'])
+print(result)
